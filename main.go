d02ac96cc558efec51067243ae03e61fe52324e1
jeonjonghyeok <tongher1685@gmail.com>
2021-05-14 16:28:58 +0900

    cli based mqtt protocol done

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/AllenDang/gform"
 	"github.com/jeonjonghyeok/chat-mqtt/mqtt"
+	"github.com/webview/webview"
 )
 
 const (
@@ -16,10 +16,46 @@ const (
 )
 
 func main() {
-	gform.Init()
-	topic := flag.String("room", "", "chatroom")
+	debug := true
+	w := webview.New(debug)
+	defer w.Destroy()
+	w.SetTitle("mqtt chat application")
+	w.SetSize(800, 600, webview.HintNone)
+
+	//topic := flag.String("room", "", "chatroom")
 	name := flag.String("name", "", "username")
 	flag.Parse()
+	/*
+		w.Bind("noop", func() string {
+			log.Println("hello")
+			return "hello"
+		})
+		w.Bind("add", func(a, b int) int {
+			return a + b
+		})
+		w.Bind("quit", func() {
+			w.Terminate()
+		})
+		//w.Navigate("https://en.m.wikipedia.org/wiki/Main_page")
+		w.Navigate(`data:text/html,
+			<!doctype html>
+			<html>
+				<body>hello</body>
+				<script>
+					window.onload = function() {
+						document.body.innerText = ` + "`hello, ${navigator.userAgent}`" + `;
+						noop().then(function(res) {
+							console.log('noop res', res);
+							add(1, 2).then(function(res) {
+								console.log('add res', res);
+							});
+						});
+					};
+				</script>
+			</html>
+		)`)
+		w.Run()
+	*/
 
 	if *topic == "" {
 		fmt.Println("Invalid setting for -room, must not be empty")
@@ -31,7 +67,7 @@ func main() {
 	}
 	fmt.Printf("Info:\n")
 	fmt.Printf("\tname:  %s\n", *name)
-	fmt.Printf("\troom:     %s\n", *topic)
+	//fmt.Printf("\troom:     %s\n", *topic)
 
 	scanner := bufio.NewScanner(os.Stdin)
 
