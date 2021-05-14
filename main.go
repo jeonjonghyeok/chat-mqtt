3939c1ec889933911ceaf48f66ecbbf61b014740
jeonjonghyeok <tongher1685@gmail.com>
2021-05-14 15:12:20 +0900

    cli based mqtt protocol

--- main.go
+++ main.go
@@ -1,25 +1,54 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
-	"log"
+	"os"
 
-	"github.com/jeonjonghyeok/chat-mqtt/server"
+	"github.com/AllenDang/gform"
+	"github.com/jeonjonghyeok/chat-mqtt/mqtt"
 )
 
 const (
-	DB_USER     = "jjh"
-	DB_PASSWORD = "zxcvasdf123"
-	DB_NAME     = "tt"
+	broker = "broker.emqx.io"
+	port   = 1883
 )
 
 func main() {
-	if err := server.ListenAndServe(server.Config{
-		Address: ":5000",
-		Url: fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-			DB_USER, DB_PASSWORD, DB_NAME),
-	}); err != nil {
-		log.Fatal(err)
+	gform.Init()
+	topic := flag.String("room", "", "chatroom")
+	name := flag.String("name", "", "username")
+	flag.Parse()
+
+	if *topic == "" {
+		fmt.Println("Invalid setting for -room, must not be empty")
+		return
+	}
+	if *name == "" {
+		fmt.Println("Invalid setting for -name, must not be empty")
+		return
+	}
+	fmt.Printf("Info:\n")
+	fmt.Printf("\tname:  %s\n", *name)
+	fmt.Printf("\troom:     %s\n", *topic)
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	client := mqtt.NewClient(broker, *name, port)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	mqtt.Sub(client, *topic)
+	fmt.Println("msg를 입력하세요")
+	var msg = ""
+	for msg != "end" {
+		fmt.Print(*name + ": ")
+		scanner.Scan()
+		msg = scanner.Text()
+		mqtt.Pub(client, *topic, msg, *name)
+
 	}
 
+	client.Disconnect(250)
 }
---
+++ mqtt/broker.go
@@ -0,0 +1,50 @@
+package mqtt
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	if string(msg.Payload()) == "end" {
+		log.Println("bye")
+		return
+	}
+	fmt.Printf("[server]: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connect lost: %v", err)
+}
+
+func NewClient(broker, name string, port int) (Client mqtt.Client) {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
+	opts.SetClientID(name)
+	opts.SetUsername("emqx")
+	opts.SetPassword("public")
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+	return mqtt.NewClient(opts)
+
+}
+
+func Pub(client mqtt.Client, topic, msg, name string) {
+	text := fmt.Sprintf("Name %s, Message %s", name, msg)
+	token := client.Publish(topic, 0, false, text)
+	token.Wait()
+	time.Sleep(time.Second)
+}
+
+func Sub(client mqtt.Client, topic string) {
+	token := client.Subscribe(topic, 1, nil)
+	token.Wait()
+}
